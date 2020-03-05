a7772c2d441f52d99a8360551ccf5e7db48ad82e
howshowwin <50402415+howshowwin@users.noreply.github.com>
2020-03-05 10:43:21 +0800

    ss

--- js/resize.js
+++ js/resize.js
@@ -14,11 +14,14 @@ $(function () {
 
 
 });
-
-
-var spcial_move = ""
-
-
+var canvasHH
+var canvasWW
+var b4_inline_style = `width:${Math.round(90 * sRatio) + "px"};height:auto;`
+var sR = 1
+var spcial_move = ''
+var inline_style = `width:${Math.round(124 * sRatio) + "px"};height:auto};`
+var popo = $('.append_box_word').offset().left
+var jiji = $('.canvas_wrapper').offset().top
 function convertSizeALL(className) {
 
     var w = 1920, h = 900;
@@ -27,12 +30,20 @@ function convertSizeALL(className) {
     sRatio = Math.min(xRatio, yRatio);
     $(className).css({ width: Math.round(w * sRatio), height: Math.round(h * sRatio) });
     $(".outside-frame").css({ width: Math.round(w * sRatio), height: Math.round(h * sRatio) });
-
+    inline_style = `width:${Math.round(124 * sRatio) + "px"};height:auto};`
+    popo = $('.append_box_word').offset().left
+    jiji = $('.canvas_wrapper').offset().top
     var reW = 1.92771084337349
     var reH = 1.31964809384164
     $("body").css("margin-left", (iw - Math.round(w * sRatio)) / 2);
-    spcial_move  = (iw - Math.round(w * sRatio)) / 2
-    $(".box").css({ width: Math.round(300 * sRatio), height: Math.round(700 * sRatio) });
+    $("body").css("margin-top", "0");
+    $("body").css("zoom", "100%");
+    canvas.setHeight(900 * sRatio);
+    canvas.setWidth(1340 * sRatio);
+    sR = Math.min(xRatio, yRatio);
+    spcial_move = (iw - Math.round(w * sRatio)) / 2
+    b4_inline_style = `width:${Math.round(90 * sRatio) + "px"};height:auto;`
+    $(".box").css({ width: Math.round(270 * sRatio), height: Math.round(680 * sRatio) });
     // $('canvas').css({ width: Math.round(1620  * sRatio), height: Math.round(700* sRatio) });
     $('.carousel-item img').css({ width: Math.round(295 * sRatio), height: Math.round(138 * sRatio) });
     $(".ans_hv,.ans2_hv,.ans3_hv").css({ width: Math.round(89.5 * sRatio), height: Math.round(44 * sRatio) });
@@ -47,26 +58,140 @@ function convertSizeALL(className) {
     $('.change_frame').css({ width: Math.round(340 * sRatio), height: Math.round(200 * sRatio) });
     $('.btn_3,.btn_4,.btn_5').css({ width: Math.round(170 * sRatio), height: Math.round(60 * sRatio), fontSize: Math.round(15 * sRatio) });
     $('.btn_6').css({ width: Math.round(60 * sRatio), height: Math.round(60 * sRatio) });
-    $("#background_canvas").css({ width: Math.round(1920 * sRatio), height: Math.round(700 * sRatio) });
-    $('.menu-item,.menu-open-button').css({ width: Math.round(80 * sRatio), height: Math.round(80 * sRatio) });
-    $('.hamburger').css({ width: Math.round(25 * sRatio), height: Math.round(3 * sRatio) });
-    $('.menu').css({ width: Math.round(300 * sRatio), height: Math.round(150 * sRatio) });
-    $('.now_color').css({ width: Math.round(100 * sRatio), height: Math.round(100 * sRatio) });
-    $('.paint_function').css({ width: Math.round(160 * sRatio), height: Math.round(30 * sRatio), fontSize: Math.round(20 * sRatio) });
-    $('.outside-frame').css({paddingLeft: Math.round(20 * sRatio) })
+    $("#background_canvas").css({ width: Math.round(1570 * sRatio), height: Math.round(678 * sRatio) });
+    $('.info_change').css({ fontSize: Math.round(60 * sRatio) })
+    $('.info_delete').css({ fontSize: Math.round(60 * sRatio) })
+    $('.outside_frame').css({ width: Math.round(1920 * sRatio), height: Math.round(900 * sRatio) })
+    $('.outside_frame_main').css({ width: Math.round(1920 * sRatio), height: Math.round(900 * sRatio) })
+
+    $('.append_box_word').css({ width: Math.round(300 * sRatio), height: Math.round(300 * sRatio) })
+    $('.canvas_wrapper ').css({ width: Math.round(1340 * sRatio), height: Math.round(900 * sRatio) })
+    canvasHH = Math.round(847 * sRatio)
+    canvasWW = Math.round(1340 * sRatio)
+    $('.next img,.pre img').css({ width: Math.round(40 * sRatio), height: Math.round(40 * sRatio) })
+    $('.ab-bb-next img,.ab-bb-pre img').css({ width: Math.round(40 * sRatio), height: Math.round(40 * sRatio) })
+    $('.classindex img').css({ width: Math.round(168 * sRatio), height: Math.round(168 * sRatio) })
+    $('.btn_choose .btn').css({ width: Math.round(90 * sRatio), height: Math.round(158 * sRatio) })
+    $('.dotbox').css({ width: Math.round(250 * sRatio), height: Math.round(30 * sRatio) })
+    $('.dot').css({ width: Math.round(30 * sRatio), height: Math.round(30 * sRatio), marginLeft: Math.round(0 * sRatio) })
+    $('.append_box_word').css({ width: Math.round(380 * sRatio), height: Math.round(550 * sRatio) });
+    $('.out-layout').css({ width: Math.round(194 * sRatio), height: Math.round(213 * sRatio) });
+    $('.layout-img').css({ width: Math.round(102 * sRatio), height: Math.round(130 * sRatio) });
+    $('.in-flex-box').css({ width: Math.round(1396 * sRatio), height: Math.round(214 * sRatio) });
+    $('.font-box').css({ width: Math.round(1280 * sRatio), height: Math.round(155 * sRatio) });
+    $('.box-pic').css({ width: Math.round(110 * sRatio), height: Math.round(120 * sRatio) });
+    $('.box-pic10').css({ width: Math.round(130 * sRatio), height: Math.round(120 * sRatio) });
+    $('.choose_area').css({ width: Math.round(630 * sRatio) });
+    $('.choose_btn').css({ width: Math.round(161 * sRatio), height: Math.round(90 * sRatio), marginRight: Math.round(10 * sRatio) });
+    $('.tool-img').css({ width: Math.round(73 * sRatio), height: Math.round(73 * sRatio), marginRight: Math.round(20 * sRatio) });
+
+    // canvas.setHeight(  canvasHH); 
+    // canvas.setWidth(  canvasWW); 
+    if ($('.dotbox').width() < 65) {
+        $('.dotbox').css({ top: "80%" })
+        $('.ab-bb-next').css({ top: "80%" })
+        $('.ab-bb-pre').css({ top: "80%" })
+        $('.next').css({ top: "80%" })
+        $('.pre').css({ top: "80%" })
+
+    } else if (($('.dotbox').width() > 60)) {
+        $('.dotbox').css({ top: "85.5%" })
+        $('.ab-bb-next').css({ top: "85.5%" })
+        $('.ab-bb-pre').css({ top: "85.5%" })
+        $('.next').css({ top: "85.5%" })
+        $('.pre').css({ top: "85.5%" })
+    }
+
+    // canvas.renderAll(); 
+    change_img = $("#defaultImg").css('width')
+    console.log("1" + change_img)
+
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+
+// if(isiOS==false){
+    let scalenow = parseInt(change_img) / parseInt(width_img)
+    image_q.forEach((obj) => {
+        console.log(obj._element.x
+        )
+
+        obj.scaleX = obj.scaleX * scalenow
+
+        obj.scaleY = obj.scaleY * scalenow
+        obj.left = obj.left * scalenow
+        obj.top = obj.top * scalenow
+        //這樣才會改變控制
+        obj.setCoords()
+        // obj.width=  obj._element.naturalWidth* scalenow
+        // obj.height=  obj._element.naturalHeigh* scalenow
+
+
+    });
+
+    canvas.discardActiveObject().renderAll()
+    // canvas.discardActiveObject().renderAll()
+    canvas.renderAll()
+// }
+
+    setword()
+    width_img = change_img
+    // width_img = $("#defaultImg").css('width')
+    if (control_btn == 4) {
+
+
+        if ($('.pic_number').length >= 1) {
+            let pic_length = $('.pic_number').length
+            let pic_app = 8 - pic_length
+            for (i = 0; i < pic_app; i++) {
+                if (control_btn == 4) {
+                    $('.append_box_word').append(
+                        `<img id="defaultImg" style=${b4_inline_style} class="pic_number grid${pic_length + i + 1}"'  draggable>`
+                    )
+                } else
+                    $('.append_box_word').append(
+
+                        `<img id="defaultImg" style=${inline_style} class="pic_number grid${pic_length + i + 1}"'  draggable>`
+                    )
+            }
+        }
+
+
+
+        $('.append_box_word').append(`
+<img id="defaultImg" style=${b4_inline_style} class="pic_number grid100" src='img/page/0.svg'  draggable>
+<img id="defaultImg" style=${b4_inline_style} class="pic_number grid101" src='img/page/1.svg'  draggable>
+<img id="defaultImg" style=${b4_inline_style} class="pic_number grid102" src='img/page/2.svg'  draggable>
+<img id="defaultImg" style=${b4_inline_style} class="pic_number grid103" src='img/page/3.svg'  draggable>
+`)
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
+// var height_img
+var width_img = $("#defaultImg").css('width')
 
-var height_img
-var width_img
+// var resize_change_items = function () {
+//     setTimeout(function () {
+//         height_img = $("#defaultImg").css('height')
+//         width_img = $("#defaultImg").css('width')
+//         console.log(height_img)
+//         console.log(width_img)
+//     }, 500)
+// }
 
-$(window).resize(function () {
-    location.reload()
+// $(window).resize(function () {
+//     resize_change_items()
 
+// })
 
-})
-setTimeout(function () {
-    height_img = $("#defaultImg").css('height')
-    width_img = $("#defaultImg").css('width')
-    console.log(height_img)
-    console.log(width_img)
-}, 100)
+// resize_change_items()

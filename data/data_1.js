87ed68a4c77a9cba4f0fa1dba1fa7fb455d1f5a3
howshowwin <50402415+howshowwin@users.noreply.github.com>
2020-03-05 15:18:33 +0800

    144

---
+++ data/data_1.js
@@ -0,0 +1,25 @@
+
+
+//第1課
+//句子
+var lesson_One_Sentence =
+    ["ta1.png",
+        "ta2.png",
+        "ta3.png",
+        "ta4.png",
+        "ta5.png",
+        "ta2.png",
+        "ta3.png",
+        "ta4.png",
+        "ta5.png",
+        "ta2.png",
+        "ta3.png",
+        "ta4.png",
+        "ta5.png"]
+
+//語詞
+var lesson_One_Words = ["tb1.png", "tb2.png", "tb3.png", "tb4.png", "tb5.png", "tb5.png", "tb6.png"]
+//生字
+var lesson_One_New_Words = ["tc1.png","tc2.png","tc3.png","tc4.png","tc5.png","tc5.png","tc6.png","tc7.png"]
+//符號
+var lesson_One_Sympol = ["td1.png","td2.png","td3.png","td4.png","td5.png","td6.png","td7.png","td2.png","td3.png","td4.png","td5.png","td6.png","td7.png"]
---
+++ data/data_9.js
@@ -0,0 +1,10 @@
+
+//第9課
+//句子
+var lesson_Nine_Sentence = ["ta1.png","ta2.png","ta3.png","ta4.png","ta5.png","ta2.png","ta3.png","ta4.png","ta5.png","ta2.png","ta3.png","ta4.png","ta5.png"]
+//語詞
+var lesson_Nine_Words = ["tb1.png", "tb2.png", "tb3.png", "tb4.png", "tb5.png", "tb5.png", "tb6.png", "tb2.png", "tb3.png", "tb4.png", "tb5.png", "tb5.png", "tb6.png"]
+//生字
+var lesson_Nine_New_Words =  ["tc1.png","tc2.png","tc3.png","tc4.png","tc5.png","tc5.png","tc6.png","tc7.png","tc4.png","tc5.png","tc5.png","tc6.png","tc7.png"]
+//符號
+var lesson_Nine_Sympol = ["td1.png","td2.png","td3.png","td4.png","td5.png","td6.png","td7.png","td2.png","td3.png","td4.png","td5.png","td6.png","td7.png"]
